091bc0518b73706ab58fd0bec5d108b056eda566
heeyeoul kim <kheeyeoul@gmail.com>
2023-07-17 21:10:31 +0900

    [complete] 로그인 기능을 위해 JWT 인증/인가 filter의 경로명 수정 및 로그인 성공 시 생성된 token을 cookie가 아니라 header에 저장

---
+++ src/main/java/com/sparta/dtogram/common/dto/ApiResponseDto.java
@@ -0,0 +1,20 @@
+package com.sparta.dtogram.common.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@ToString // 출력 시 각 field명을 포함시키는 annotation
+public class ApiResponseDto {
+    private int statusCode;
+    private String statusMessage;
+
+    @Builder // 해당 annotation이 적힌 field는 반드시 초기화가 되어있어야 한다.
+    public ApiResponseDto(String statusMessage, int statusCode){
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    }
+}
