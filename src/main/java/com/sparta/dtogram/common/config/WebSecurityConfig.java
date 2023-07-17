11ad829265d602e9e50c85047e49d9458df305d0
heeyeoul kim <kheeyeoul@gmail.com>
2023-07-17 20:23:33 +0900

    [update] resources 폴더의 자료들과 메인페이지('/'), 그리고 '/api/user/'로 시작하는 모든 요청에 대한 접근 허용

--- src/main/java/com/sparta/dtogram/common/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/dtogram/common/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.sparta.dtogram.common.jwt.JwtAuthorizationFilter;
 import com.sparta.dtogram.common.jwt.JwtUtil;
 import com.sparta.dtogram.common.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -54,7 +55,9 @@ public class WebSecurityConfig {
 
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
-                        .requestMatchers(HttpMethod.POST, "/api/user/**").permitAll() // '/api/user'로 시작하는 POST 요청 모두 접근 허가
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                        .requestMatchers("/api/user/**").permitAll() // '/api/user'로 시작하는 모든 요청 접근 허가
                         .requestMatchers(HttpMethod.GET, "/api/post", "/api/post/**").permitAll() // '/api/post' 요청과 '/api/post/**"로 시작하는 POST 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
