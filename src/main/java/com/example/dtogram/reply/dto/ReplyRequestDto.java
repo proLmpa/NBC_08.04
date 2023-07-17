ecdb9797d0d44afaa701b0d68aea1a98d5948e2b
Hyeon woo Kim <wooin.dev@gmail.com>
2023-07-17 16:28:46 +0900

    꼬인 파일 정리

--- src/main/java/com/example/dtogram/reply/dto/ReplyRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.dtogram.reply.dto;
-
-import lombok.Getter;
-
-@Getter
-public class ReplyRequestDto {
-    private String content;
-}
--- src/main/java/com/example/dtogram/reply/repository/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dtogram.reply.repository;
-
-import com.example.dtogram.reply.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
