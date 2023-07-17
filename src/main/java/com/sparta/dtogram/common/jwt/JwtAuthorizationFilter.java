bdec85237cb2e19e2cbe2440fa8379eb5da72c61
heeyeoul kim <kheeyeoul@gmail.com>
2023-07-17 21:11:20 +0900

    [complete+@] Header 방식 적용에 따라 Cookie 저장을 위해 사용했던 method 삭제

--- src/main/java/com/sparta/dtogram/common/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/dtogram/common/jwt/JwtAuthorizationFilter.java
@@ -36,7 +36,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         if (StringUtils.hasText(tokenValue)) {
             // JWT 토큰 substring
             tokenValue = jwtUtil.substringToken(tokenValue);
-            log.info(tokenValue);
+//            log.info(tokenValue);
 
             if (!jwtUtil.validateToken(tokenValue)) {
                 log.error("Token Error");
