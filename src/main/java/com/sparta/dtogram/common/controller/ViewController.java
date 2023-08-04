fb7e64f0784b808d44de4da693067b162ca5d6b5
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-04 12:19:12 +0900

    [refactor] ViewController 리팩토링

--- src/main/java/com/sparta/dtogram/common/controller/ViewController.java
+++ src/main/java/com/sparta/dtogram/common/controller/ViewController.java
@@ -1,8 +1,6 @@
 package com.sparta.dtogram.common.controller;
 
-import com.sparta.dtogram.common.security.UserDetailsImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public String home() {
         return "index";
     }
 
