4d822a4c62f56313bed5aef21b970a7b6eb41457
heeyeoul kim <kheeyeoul@gmail.com>
2023-08-04 15:50:34 +0900

    [init] springdoc-openai-swagger 적용 위한 환경 설정

--- src/main/java/com/sparta/dtogram/common/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/dtogram/common/config/WebSecurityConfig.java
@@ -61,8 +61,8 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/user/**").permitAll()// '/api/user'로 시작하는 모든 요청 접근 허가
                         .requestMatchers(HttpMethod.GET, "/api/post", "/api/post/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/reply", "/api/reply/**").permitAll()
-
                         .requestMatchers(HttpMethod.GET, "/api/profile/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
